c282db9383ddaee501f3ef9b1dbce5396e51c3ff
Troy <twc17@pitt.edu>
2015-08-23 10:01:57 -0400

    Comments!

--- a/helper.py
+++ a/helper.py
@@ -4,18 +4,34 @@
 Title: helper.py
 Language: python3
 Author: Troy Caro <twc17@pitt.edu>
-Purpose: Helper functions for port_serch.py and mac_search.py
+Project Purpose: To provide the Helpdesk with a more accurate tool for getting information about end-user devices
+File Purpose: Helper functions for port_serch.py and mac_search.py
 """
 
+# Import statements
 import sys
 import socket
 import paramiko
-
-# Here is where the magic starts
 import cgi
 import cgitb
 
-# INPUT
+"""
+Description:
+    Based on the port description, this function will attempt to locate the correct switch,
+    and assemble a command to show the status of the interface associated with the 
+    port description
+
+    The function will try to determine the switch based off of the center portion of the
+    port description. Sticking with the PittNET naming convention, it will first try
+    to connect to a c3850, then a c3750. If both of those lookups fail, the program will exit
+
+Variables:
+    port - The port description
+
+Returns:
+    hst - The hostname of the switch
+    cmd - The command to run on the switch
+"""
 def port_address(port):
     port_num = port.split("-")
     cmd  = "sh int status | incl " + port_num[2] 
@@ -32,34 +48,49 @@ def port_address(port):
             print("<p> Hmm, couldn't find the switch based on that port address :/ </p>")
             sys.exit(1)
 
-# EXECUTE
-def check_host(hst):
-    """
-    Trys to resolve the hostname
+"""
+Description:
+    Check to see if a given hostname resolves to an IP address.
 
-    Pre-conditions:
-            Hostname is formatted correctly
-    Post-conditions:
-            Returns 1 if the hostname resolves, 0 if it does not
-    """
+Variables:
+    hst - The hostname to check
+
+Returns:
+    Will return 1 if the hostname resolves, 0 if it does not
+"""
+def check_host(hst):
     try:
         socket.gethostbyname(hst)
         return 1
     except socket.error:
         return 0
 
+"""
+Description:
+    Runs a command against a host over SSH protocol on default port 22
+
+Variables:
+    hst - The host to connect to
+    usr - The username used to authenticate
+    passwd - Password for 'usr'
+    cmd - The command to run on the host machine
+
+Returns:
+    Output of command run on remote host
+"""
 def execute(hst, usr, passwd, cmd):
-    """
-    Executes command on ssh server and returns output
-    """
     ssh = paramiko.SSHClient()
+    # Set AutoAddPolicy so that we are not prompted to add new hosts to know_hosts file
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     try:
+        # All PittNET switches run SSH on default port 22
         ssh.connect(hst, 22, usr, passwd)
+        # stdin, stdout, sterr are all set here, could write if needed
         stdin, stdout, stderr = ssh.exec_command(cmd)
         output = stdout.read().strip()
     except paramiko.ssh_exception.AuthenticationException as e:
         print("<p> Oops, looks like you entered your username or password wrong :/ </p>")
         sys.exit(1)
 
+    # stdout is written in 'bytes'. Needs to be decoded before priting
     return output.decode(encoding='UTF-8')
--- a/port_search.py
+++ a/port_search.py
@@ -4,12 +4,14 @@
 Title: port_search.py
 Language: python3
 Author: Troy Caro <twc17@pitt.edu>
-Purpose: Search Cisco Catalyst series switches on PittNET by port description
+Project Purpose: To provide the Helpdesk with a more accurate tool for getting information about end-user devices
+File Purpose: Search Cisco Catalyst series switches on PittNET by port description
 """
 
+# Import everything from helper, we'll need it!
 from helper import *
 
-# Start
+# Start output to html page
 print("Content-type:text/html\r\n\r\n")       
 
 # Create instance of FieldStorage
@@ -20,6 +22,7 @@ port = form.getvalue('port_address')
 usr = form.getvalue('user_name')
 passwd = form.getvalue('password')
 
+# Setup CSS and page title
 print("""
 <html>
 
@@ -38,26 +41,31 @@ print("<h1>Results for <b>" + port + "</b></h1>" )
 # Check to see if the port maps to a switch before going any further
 # port_address() is set to exit if it can't find the switch
 hst, cmd = port_address(port)
-output = execute(hst, usr, passwd, cmd)
 
+# If the output of execute is empty, then we know that we have the correct switch, but the port description isn't there.
+# We'll ask the user if they want to search this switch by MAC address
+output = execute(hst, usr, passwd, cmd)
 if (output == ""):
     print("<p> I found the switch, but not the port :/ </p>")
-    # Haven't quite figured out the best way to handle this yet
     print("<p> If the device is connected, you can try searching by MAC address <a href='http://136.142.3.230/mac_search.html?port_address=" + port + "'>here</a>. </p>")
     sys.exit(1)
 
+# Split output and get interface status (connected, notconnected, disabled, etc)
 output = output.split()
 mac_status = output[2]
 
+# If the interface is showing connected, we'll want to also display the connected MAC address
 if (mac_status == "connected"):
     mac_test = execute(hst, usr, passwd, ("sh mac add | incl " + output[0]))
+    # This is a weird case where the interface is showing as connected, but the MAC address isn't showing up
     if (mac_test == ""):
         mac_status = "Hmm..? :/"
     else:
-        mac_status = mac_status.split()[1]
+        mac_status = mac_test.split()[1]
 else:
     mac_status = "None"
 
+# Output results to html page
 print("<p><b>Port Address: </b>" + output[1] + "</p>")
 print("<p><b>Status: </b>" + output[2] + "</p>")
 print("<p><b>VLAN: </b>" + output[3] + "</p>")
